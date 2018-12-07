77dcadcc6e26190ee2c34f586e3bbef9a8cea6a4
yaoruozi <2319695901@qq.com>
2018-12-07 23:50:45 +0800

    java8新建

---
+++ Project001/src/com/demo/one/LambdaTest01.java
@@ -0,0 +1,7 @@
+package com.demo.one;
+
+public class LambdaTest01 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Project001/src/com/demo/one/Test.java
@@ -0,0 +1,19 @@
+package com.demo.one;
+
+/**
+ * lambda进阶练习
+ *
+ * */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+    //使用最原始的匿名内部类进行两个int型数据大小的比较
+    @org.junit.Test
+    public void test(){
+        
+    }
+
+
+
+}
