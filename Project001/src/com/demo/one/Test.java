9f7f0057e1ae22ad905f5898e77c2fd47890bb84
yaoruozi <2319695901@qq.com>
2018-12-08 00:07:40 +0800

    添加lambda表达式代码

--- Project001/src/com/demo/one/Test.java
+++ Project001/src/com/demo/one/Test.java
@@ -1,19 +1,34 @@
 package com.demo.one;
 
+import java.util.Comparator;
+import java.util.TreeSet;
+
 /**
  * lambda进阶练习
- *
- * */
+ */
 public class Test {
     public static void main(String[] args) {
 
     }
+
     //使用最原始的匿名内部类进行两个int型数据大小的比较
     @org.junit.Test
-    public void test(){
-        
+    public void test() {
+        Comparator<Integer> com = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.compare(o1, o2);
+            }
+        };
+        TreeSet<Integer> treeSet = new TreeSet<>(com);
     }
 
+    //对上面的方法进行升级，使用lambda表达式
+    @org.junit.Test
+    public void testLambda() {
+        Comparator<Integer> com = (x, y) -> Integer.compare(x, y);
+        TreeSet<Integer> int_set = new TreeSet<>(com);
+    }
 
 
 }
